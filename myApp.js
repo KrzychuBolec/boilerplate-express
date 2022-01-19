7e2c8a0b5e61f20f86a9a808492397eaa8b5d1d1
zagorny95 <krzysztof.podworny@wp.pl>
2022-01-19 18:41:53 +0100

    Changes

--- myApp.js
+++ myApp.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
+const os = require("os")
+const user = os.userInfo()
 
-console.log("Hello World")
+console.log(os.type())
 
 let handler = (req,res) =>{
     res.sendFile(__dirname + "/views/index.html")

2328296c13e002f9d0fb7ff02881c109e16b87c2
zagorny95 <krzysztof.podworny@wp.pl>
2022-01-19 19:41:57 +0100

    Added logger

--- myApp.js
+++ myApp.js
@@ -7,6 +7,18 @@ var env = require('dotenv').config()
 
 console.log(os.type())
 
+let logger = (req,res,next) =>{
+    
+    let method = req.method
+    let path = req.path
+    let ip = req.ip
+    
+    console.log(method + " "+path + " - "+ip)
+    next()
+}
+
+app.use(logger)
+
 app.use("/public",express.static(__dirname + "/public"))
 
 let handler = (req,res) =>{
